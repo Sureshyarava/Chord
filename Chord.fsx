0fd1246f3c15f1268d9f9899749f990f9d57bc88
Suresh <shashankyarava1234@gmail.com>
2023-11-02 03:14:53 -0400

    Added comments

--- Chord.fsx
+++ Chord.fsx
@@ -1,9 +1,9 @@
-open System.Collections.Generic
-open System
-open System.Threading
 #r "nuget: Akka"
 
 open Akka.Actor
+open System
+open System.Collections.Generic
+open System.Threading
 
 // Function to generate a unique random number and update the set
 let generateUniqueRandomNumber (maxValue: int) (mutableSet: HashSet<int>) =
@@ -19,20 +19,20 @@ let generateUniqueRandomNumber (maxValue: int) (mutableSet: HashSet<int>) =
     randomNumber
 
 let m = 20
-let maxvalue = int32 (pown 2 m - 1) // max range of values allowed to give for a node
+let maxvalue = int32 (pown 2 m - 1) // Maximum range of values allowed for a node
 
-let keyList =List<int>()
+let keyList = List<int>()
 
-let mutableSet = HashSet<int>() // Used to store and tell if the random number is already been used
+let mutableSet = HashSet<int>() // Used to store and determine if a random number has already been used
 
-let myList = LinkedList<int>() //Used to store Node keys
+let myList = LinkedList<int>() // Used to store Node keys
 let mutable numberOfNodes = 0
 let mutable numberOfRequests = 0
 
 let args = System.Environment.GetCommandLineArgs()
-if args.Length >= 3 then // The first argument is the File name 
-    numberOfNodes <- int args.[2] // The second argument is the number of nodes
-    numberOfRequests <- int args.[3] // The third argument is the number of requests that need to be done 
+if args.Length >= 3 then
+    numberOfNodes <- int args.[2] // Number of nodes
+    numberOfRequests <- int args.[3] // Number of requests
 else
     printfn "Please provide two command-line arguments: NumberOfNodes and NumberOfRequests."
 
@@ -55,8 +55,8 @@ let mutable globalHead : int = -1
 let mutable nodeList = Dictionary<int, chord_Node>()
 
 let mutable nodeData = List<int>()
-// Function to create a circular linked list node
 
+// Function to create a circular linked list node
 let calculateNumberOfHops (startNodeKey: int) (lookupKey: int) =
     let rec loop currentNodeKey hops =
         if currentNodeKey = lookupKey then hops
@@ -77,8 +77,9 @@ let calculateNumberOfHops (startNodeKey: int) (lookupKey: int) =
 
     loop startNodeKey 0
 
-let mutable hopCount =0;
+let mutable hopCount = 0
 
+// Class for actors
 type MyActor(requestsCountdown: CountdownEvent) =
     inherit ReceiveActor()
 
@@ -91,37 +92,37 @@ type MyActor(requestsCountdown: CountdownEvent) =
             while counter < numberOfRequests do
                 do! Async.Sleep(1000) // Sleep for 1 second
                 let nodeValue = int selfRef.Path.Name
-                let key=calculateNumberOfHops nodeValue keyList[counter]
-                hopCount <- hopCount+key
+                let key = calculateNumberOfHops nodeValue keyList[counter]
+                hopCount <- hopCount + key
                 counter <- counter + 1
 
             printfn "%s has finished all requests" (selfRef.Path.Name)
             if requestsCountdown.Signal() then
-                printfn "The average number of hops is %f" (float hopCount/ float (numberOfNodes*numberOfRequests))
+                printfn "The average number of hops is %f" (float hopCount / float (numberOfNodes * numberOfRequests))
                 Environment.Exit(0)
-            } |> Async.Start
+        } |> Async.Start
 
 // Function to create an actor with a custom name
 let createActor (actorSystem: ActorSystem) (name: string) =
     let actorref = actorSystem.ActorOf(Props.Create(fun () -> MyActor(requestsCountdown)), name)
     actorref
 
-for i=1 to numberOfRequests do
-    let key=generateUniqueRandomNumber maxvalue mutableSet
+for i = 1 to numberOfRequests do
+    let key = generateUniqueRandomNumber maxvalue mutableSet
     keyList.Add(key)
 
-let getSuccesor (key:int32) =
+let getSuccesor (key: int32) =
     let mutable successor = -1
-    for k = 0 to nodeData.Count-2 do
-        if key > nodeData.[k] && key <= nodeData.[k+1] && successor = -1 then
-            successor <- nodeData.[k+1]
+    for k = 0 to nodeData.Count - 2 do
+        if key > nodeData.[k] && key <= nodeData.[k + 1] && successor = -1 then
+            successor <- nodeData.[k + 1]
     if successor = -1 then
         successor <- nodeData.[0]
     successor
 
-let UpdateFingerTable(id:int) =
-    for k=1 to m do
-        let key1= id+int32 (pown 2 (k - 1))
+let UpdateFingerTable (id: int) =
+    for k = 1 to m do
+        let key1 = id + int32 (pown 2 (k - 1))
         let value1 = getSuccesor (key1 % (int32 (pown 2 m))) 
         nodeList.[id].FingerTable[key1] <- value1
 
@@ -149,32 +150,30 @@ let join (name: IActorRef) (data: int) =
         }
     nodeList.[newNode.key] <- newNode 
     if data < nodeData[0] || data > nodeData[nodeData.Count-1] then
-        let temp=nodeList[nodeData[nodeData.Count-1]].Next
-        newNode.Next<-temp
-        nodeList[nodeData[nodeData.Count-1]].Next<- Some newNode
+        let temp = nodeList[nodeData[nodeData.Count-1]].Next
+        newNode.Next <- temp
+        nodeList[nodeData[nodeData.Count-1]].Next <- Some newNode
         if data < nodeData[0] then
-            nodeData.Insert(0,data)
+            nodeData.Insert(0, data)
         else
             nodeData.Add(data)
     else
-        let mutable temp1=true
-        for i=1 to nodeData.Count-1 do
-            if temp1 && nodeData[i-1]<data && data <nodeData[i] then
-                let temp= nodeList[nodeData[i-1]].Next
-                newNode.Next<-temp
-                nodeList[nodeData[i-1]].Next <- Some newNode
-                nodeData.Insert(i,data)
+        let mutable temp1 = true
+        for i = 1 to nodeData.Count - 1 do
+            if temp1 && nodeData[i - 1] < data && data < nodeData[i] then
+                let temp = nodeList[nodeData[i - 1]].Next
+                newNode.Next <- temp
+                nodeList[nodeData[i - 1]].Next <- Some newNode
+                nodeData.Insert(i, data)
                 temp1 <- false
     for k in nodeData do
         UpdateFingerTable k
 
-
-
-let system =
-    ActorSystem.Create("ChordSystem")
+// Create the ActorSystem
+let system = ActorSystem.Create("ChordSystem")
 
 for i in 1 .. numberOfNodes do
-    let uniqueNumber = generateUniqueRandomNumber maxvalue mutableSet    // Create actors with custom names
+    let uniqueNumber = generateUniqueRandomNumber maxvalue mutableSet
     let actorName = string uniqueNumber
     let actor = createActor system actorName
     if i = 1 then
